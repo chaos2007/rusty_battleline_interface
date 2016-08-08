ffcfda73e971d01b9ba367dc289dcf0d07dc0318
Don Pollitz <chaos2007@gmail.com>
2016-08-07 21:02:43 -0500

    All parsing finished, need to fix warnings and add combined game state.

--- src/lib.rs
+++ src/lib.rs
@@ -21,12 +21,19 @@ pub enum Message {
     Blank,
     PlayerDirection{ direction: Direction},
     ColorNames{ c1:String, c2:String, c3:String, c4:String, c5:String, c6:String },
-    FlagClaimStatus{ c1:ClaimStatus, c2:ClaimStatus, c3:ClaimStatus, 
-        c4:ClaimStatus, c5:ClaimStatus, c6:ClaimStatus, 
-        c7:ClaimStatus, c8:ClaimStatus, c9:ClaimStatus, },
-    FlagStatus,//{ number, direction, cards},
+    FlagClaimStatus{ flagsClaimed: Vec<ClaimStatus> }, 
+    FlagStatus{flagNum:u32, direction:Direction, cards:Vec<Card>},
     OpponentPlay{ number: i32, card: Card },
+    PlayerHand{ direction:Direction, cards: Vec<Card>},
     PlayCard,
+}
+
+fn convert_direction(message: &str) -> Direction {
+    match message {
+        "north" => Direction::North,
+        "south" => Direction::South,
+        e => panic!("{} is not a direction flag.", e),
+    }
 }
 
 fn convert_claim_status(message: &str) -> ClaimStatus {
@@ -38,14 +45,32 @@ fn convert_claim_status(message: &str) -> ClaimStatus {
     }
 }
 
+fn convert_string_to_card(message: &str) -> Card {
+    let split: Vec<&str>  = message.split(',').collect();
+    let b = String::from(split[0]);
+    let c = split[1].parse::<i32>().unwrap();
+    Card{ color:b, number:c}
+}
+
 pub fn parse_message(message: String) -> Message {
-/*    
-    let playerHandMessage = Regex::new(r"player (north|south) hand( \S+,\d+)*").unwrap();
-    let flagCardsMessage = Regex::new(r"flag ([1-9]) cards (north|south)( \S+,\d+)*").unwrap();
-    */
     let split: Vec<&str> = message.split_whitespace().collect();
-    match split.as_slice() {
+    match &split[..] {
         &["go", "play-card"] => Message::PlayCard,
+        &["player", direction, "hand", ref cards..] if cards.len() <= 7 && (direction == "north" || direction == "south") => {
+            let mut playerCards = Vec::new();
+            for i in cards {
+                playerCards.push(convert_string_to_card(i));
+            }
+            Message::PlayerHand{ direction:convert_direction(direction), cards:playerCards}
+        },
+        &["flag", flag, "cards", direction, ref cards..] if flag >= "1" && flag <= "9" && (direction == "north" || direction == "south")  => {
+            let mut flagsCards = Vec::new();
+            let a = flag.parse::<u32>().unwrap();
+            for i in cards {
+                flagsCards.push(convert_string_to_card(i));
+            }
+            Message::FlagStatus{flagNum:a, direction:convert_direction(direction), cards:flagsCards}
+        },
         &["player", direction, "name"] if direction == "north" || direction == "south" => {
             match direction {
                 "north" => Message::PlayerDirection{ direction: Direction::North },
@@ -54,27 +79,25 @@ pub fn parse_message(message: String) -> Message {
         },
         &["opponent", "play", flag, card] if flag >= "1" && flag <= "9" => {
             let a = flag.parse::<i32>().unwrap();
-            let split: Vec<&str>  = card.split(',').collect();
-            let b = String::from(split[0]);
-            let c = split[1].parse::<i32>().unwrap();
-            Message::OpponentPlay{ number: a, card: Card{ color: b, number:c}}
+            Message::OpponentPlay{ number: a, card: convert_string_to_card(card)}
         },
         &["colors", c1,c2,c3,c4,c5,c6] => {
             Message::ColorNames{ c1:String::from(c1), c2:String::from(c2), 
                 c3:String::from(c3), c4:String::from(c4), c5:String::from(c5), 
                 c6:String::from(c6)}
         },
-        &["flag", "claim-status", c1,c2,c3,c4,c5,c6,c7,c8,c9] => {
-            let c1 = convert_claim_status(c1);
-            let c2 = convert_claim_status(c2);
-            let c3 = convert_claim_status(c3);
-            let c4 = convert_claim_status(c4);
-            let c5 = convert_claim_status(c5);
-            let c6 = convert_claim_status(c6);
-            let c7 = convert_claim_status(c7);
-            let c8 = convert_claim_status(c8);
-            let c9 = convert_claim_status(c9);
-            Message::FlagClaimStatus{c1:c1, c2:c2, c3:c3, c4:c4, c5:c5, c6:c6, c7:c7, c8:c8, c9:c9}
+        &["flag", "claim-status", ref flagClaims..] if flagClaims.len() == 9 => {
+            let mut claims = Vec::new();
+            for i in flagClaims {
+                claims.push(convert_claim_status(i));
+            }
+            Message::FlagClaimStatus{flagsClaimed:claims}
+        },
+        &["player", direction, hand,  ref cards..] => {
+            Message::Blank
+        },
+        &["flag", number, "cards", direction, ref cards..] => {
+            Message::Blank
         },
         _ => Message::Blank,
     }
@@ -147,14 +170,38 @@ mod test_parsing_messages{
     
     #[test]
     fn flag_claim_message() {
+        let claimVec = vec![ClaimStatus::North, ClaimStatus::South, ClaimStatus::Unclaimed,
+                            ClaimStatus::Unclaimed, ClaimStatus::South, ClaimStatus::North,
+                            ClaimStatus::South, ClaimStatus::North, ClaimStatus::Unclaimed];
+            
         let x = parse_message(String::from("flag claim-status north south unclaimed unclaimed south north south north unclaimed"));
         match x {
-            Message::FlagClaimStatus{ c1:ClaimStatus::North, c2:ClaimStatus::South,
-                c3:ClaimStatus::Unclaimed, c4:ClaimStatus::Unclaimed, c5:ClaimStatus::South,
-                c6:ClaimStatus::North, c7:ClaimStatus::South,
-                c8:ClaimStatus::North, c9:ClaimStatus::Unclaimed } => {
-                },
+            Message::FlagClaimStatus{ flagsClaimed: claimVec } => {
+            },
             e => panic!("Wrong Card type."),
+        }
+    }
+
+    #[test]
+    fn flag_card_message() {
+        let cards = vec![Card{color:String::from("red"), number:3}, Card{color:String::from("blue"), number:9}];
+        let x = parse_message(String::from("flag 3 cards north red,3 blue,9"));
+        match x {
+            Message::FlagStatus{flagNum:3, direction:Direction::North, cards:cards} => {
+            },
+            _ => panic!("Wrong Card type."),
+        }
+    }
+
+    #[test]
+    fn player_hand_message() {
+        let cards = vec![Card{color:String::from("red"), number:3}, Card{color:String::from("blue"), number:6},
+                         Card{color:String::from("green"), number:4}];
+        let x = parse_message(String::from("player north hand red,3 blue,6 green,4"));
+        match x {
+            Message::PlayerHand{direction:Direction::North, cards:cards} => {
+            },
+            _ => panic!("Wrong Card type."),
         }
     }
 }
